312ff6f2c806697512e6c06541311cfd6c13b6d6
gorira-tatsu <tatsuhiko.shigoto@gmail.com>
2021-02-09 21:49:17 +0900

    mysql wait complete and dockerfile best pructice

--- main_run.sh
+++ main_run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-  sleep 10
-  export FLASK_APP=main.py
-  export FLASK_ENV=development
-  flask run --host=0.0.0.0 --port 5000
+
+until mysqladmin ping -h db --silent; do
+  echo 'waiting for mysqld to be connectable...'
+  sleep 2
+done
+
+echo " go app is started!"
+export FLASK_APP=main.py
+export FLASK_ENV=development
+flask run --host=0.0.0.0 --port 5000
