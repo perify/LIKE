cb2aa90c6ad505d069f54fa17c8960ea7dff786f
gorira-tatsu <tatsuhiko.shigoto@gmail.com>
2021-02-14 19:20:46 +0900

    flask-login

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import request, render_template, Flask
-
+from flask import request, render_template, redirect, url_for, Flask
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
+import User
 import tlsql
 
 app = Flask(__name__)
 
+app.secret_key = b'jugemjugemgokonosurikirekaijarisuigyono'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_status = []
+
 
 @app.route('/')
+@login_required
 def home_action():
     tweets = []
     for i in tlsql.view():
@@ -15,22 +23,20 @@ def home_action():
 
 @app.route('/', methods=['POST'])
 def tweet_action():
-    tweets = []
     tlsql.tweet(request.form['message'])
-    for i in tlsql.view():
-        tweets.append(i)
-    return render_template('home.html', tweets=tweets)
+    return home_action()
 
 
 @app.route('/login')
-def login_form():
+def login():
     return render_template('login.html')
 
 
 @app.route('/login', methods=['POST'])
 def login_action():
     if tlsql.login(request.form['username'], request.form['password']) == "ok":
-        return home_action()
+        login_user(request.form['username'],)
+        return redirect(url_for('home_action'))
     return render_template('login.html', unmatch=True)
 
 
@@ -43,6 +49,11 @@ def signup_form():
 def signup_action():
     tlsql.user_insert(request.form['username'], request.form['password'], request.form['Email'])
     return home_action()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User()
 
 
 if __name__ == "__main__":
